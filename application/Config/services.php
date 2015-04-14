78225e5326e418b1ee1086934d79113097703eb8
Lonnie Ezell <lonnieje@gmail.com>
2015-04-13 22:57:20 -0500

    Renamed providers config file to services to better define the actual purpose.

---
+++ application/Config/services.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file contains a map of namespaced classes and their aliases.
+ * These are used by the CI class to provide instances of classes.
+ *
+ * The 'alias' (key) is how you will reference the class instance through CI.
+ * The class name (value) is the fully namespaced class name to use.
+ * If you want to substitute a different class in place of the current one,
+ * just change the name of the class to the fully namespaced class you
+ * want to use.
+ *
+ * Examples:
+ *      $ci = \CodeIgniter\CI::getInstance();
+ *      $bm = $ci->benchmark;
+ *      $ci->benchmark->mark('some_mark_start');
+ */
+
+$config['services'] = [
+
+    // alias            class name
+    //--------------------------------------------------------------------
+
+    // The core CodeIgniter files
+    'benchmark'     => '\CodeIgniter\Benchmark',
+    'config'        => '\CodeIgniter\Config',
+
+    // Your custom files can be added here.
+];
